4b7ef9cc2549a34227f681b0f5f6503d15c7e51d
David Lai <davidlatwe@gmail.com>
2021-01-29 04:48:29 +0800

    fix patched rez bin not in egg-info, and use namespace package

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import sys
 
 
 try:
-    from setuptools import setup, find_packages, Command
-    from setuptools.command import build_py, install_data
+    from setuptools import setup, find_packages, find_namespace_packages
+    from setuptools.command import install_lib
 except ImportError:
     print("install failed - requires setuptools", file=sys.stderr)
     sys.exit(1)
@@ -25,38 +25,41 @@ sys.path.insert(0, src_path)
 from ship._entry_points import get_specifications
 
 
-class PatchRezBins(Command):
+class InstallLibWithRezBinsPatch(install_lib.install_lib):
     """Just a wrapper to get the install data into the egg-info"""
 
     def initialize_options(self):
-        self.build_dir = None
+        super(InstallLibWithRezBinsPatch, self).initialize_options()
         self.outfiles = []
 
-    def finalize_options(self):
-        self.set_undefined_options('build',
-                                   ('build_scripts', 'build_dir'))
+    def get_outputs(self):
+        outfiles = super(InstallLibWithRezBinsPatch, self).get_outputs()
+        return outfiles + self.outfiles
 
     def run(self):
+        super(InstallLibWithRezBinsPatch, self).run()
+
         rez_bin_scripts = self._patch_rez_binaries() or []
         if self.dry_run:
             return
+
+        self.announce("Patching rez bin tools", level=3)
 
         dest_bin_path = os.path.join(os.path.dirname(sys.executable), "rez")
         for script in rez_bin_scripts:
             dst = os.path.join(dest_bin_path, os.path.basename(script))
             self.outfiles.append(dst)
             self.copy_file(script, dst)
-        # TODO: not recorded in egg
-
-    def get_outputs(self):
-        return self.outfiles
 
     def _patch_rez_binaries(self):
+        # referenced from rez's install.py
         try:
             from rez.vendor.distlib.scripts import ScriptMaker
         except Exception as e:
-            print("Rez binary tool not patched: %s" % str(e))
+            self.announce("Skip patching rez bin tools: %s" % str(e), level=3)
             return
+
+        self.announce("Creating rez bin tools", level=3)
 
         dest_bin_path = os.path.join(self.build_dir, "rez_bins")
         self.mkpath(dest_bin_path)
@@ -79,13 +82,6 @@ class PatchRezBins(Command):
         return scripts
 
 
-class BuildPyCommand(build_py.build_py):
-    """Custom build command."""
-    def run(self):
-        build_py.build_py.run(self)
-        self.run_command("install_data")
-
-
 setup(
     name="ship",
     version="0.1.0",
@@ -95,16 +91,15 @@ setup(
     author_email="davidlatwe@gmail.com",
     license="LGPL",
     cmdclass={
-        "install_data": PatchRezBins,
-        "build_py": BuildPyCommand,
+        "install_lib": InstallLibWithRezBinsPatch,
     },
     entry_points={
         "console_scripts": get_specifications().values()
     },
-    namespace_packages=["rez.cli"],
+    namespace_packages=["rez", "rez.cli"],
     package_dir={"": "src"},
-    packages=find_packages("src"),
-    package_data={"rez": ["cli/ship.json"]},
+    packages=find_namespace_packages("src"),
+    package_data={"rez.cli": ["*.json"]},
     include_package_data=True,
     zip_safe=False,
 )
