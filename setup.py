514bffca2156290bb9cd34a1e4a890a927775bf0
David Lai <davidlatwe@gmail.com>
2021-03-15 22:26:34 +0800

    support option 'install_scripts'

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os.path
 import sys
 
 
+from distutils.command import install_data
 try:
     from setuptools import setup, find_packages
     from setuptools.command import build_py
@@ -25,6 +26,18 @@ sys.path.insert(0, src_path)
 from rezbefoo._entry_points import get_specifications
 from rezbefoo._version import version
 
+
+def patch_production_scripts(target_dir):
+    from rez.vendor.distlib.scripts import ScriptMaker
+    maker = ScriptMaker(source_dir=None, target_dir=target_dir)
+    maker.executable = sys.executable
+    scripts = maker.make_multiple(
+        specifications=get_specifications().values(),
+        options=dict(interpreter_args=["-E"])
+    )
+    return scripts
+
+
 with open(os.path.join(source_path, "README.md")) as f:
     long_description = f.read()
 
@@ -41,81 +54,68 @@ setup_kwargs = dict(
     author_email="davidlatwe@gmail.com",
     description="Demoing Rez's application plugin",
     long_description=long_description,
+    # install_requires=[
+    #     "rez>=2.78.0",  # on development
+    # ],
 )
 
 
-class BuildPyWithRezBinsPatch(build_py.build_py):
+class RezBuildPy(build_py.build_py):
 
     def run(self):
         build_py.build_py.run(self)
         self.patch_rez_binaries()
 
     def _append(self, data_files):
-        """Append `data_files` into `distribution.data_files`
-
-        Just like how additional files be assigned with setup(data_files=[..]),
-        but for those extra files that can only be created in build time, here
-        is the second chance.
-
-        The `data_files` specifies a sequence of (directory, files) pairs in
-        the following way:
-
-            setup(...,
-                data_files=[('config', ['foo/cfg/data.cfg'])],
-            )
-
-        Each (directory, files) pair in the sequence specifies the installation
-        directory and the files to install there.
-
-        So in the example above, the file `data.cfg` will be installed to
-        `config/data.cfg`.
-
-        IMPORTANT:
-        The directory MUST be a relative path. It is interpreted relative to
-        the installation prefix (Python’s sys.prefix for system installations;
-        site.USER_BASE for user installations).
-
-        @param data_files: a sequence of (directory, files) pairs
-        @return:
-        """
-        # will be picked up by `distutils.command.install_data`
         if self.distribution.data_files is None:
-            self.distribution.data_files = data_files
-        else:
-            self.distribution.data_files += data_files
+            self.distribution.data_files = []
+        self.distribution.data_files += data_files
 
     def patch_rez_binaries(self):
-        from rez.vendor.distlib.scripts import ScriptMaker
-
-        self.announce("Generating rez bin tools...", level=3)
-
         # Create additional build dir for binaries, so they won't be handled
         # as regular builds under "build/lib".
         build_path = os.path.join("build", "rez_bins")
         self.mkpath(build_path)
+        production_scripts = patch_production_scripts(build_path)
+        self._append([
+            # We don't know script install dir at this moment, therefore we
+            # use a placeholder and pickup later.
+            ("_production_script_:rez", production_scripts)
+        ])
 
-        # Make binaries, referenced from rez's install.py
-        maker = ScriptMaker(
-            source_dir=None,
-            target_dir=build_path
+
+class InstallData(install_data.install_data):
+
+    def initialize_options(self):
+        install_data.install_data.initialize_options(self)
+        self.script_dir = None
+
+    def finalize_options(self):
+        install_data.install_data.finalize_options(self)
+        self.set_undefined_options(
+            'install', ('install_scripts', 'script_dir'),
         )
-        maker.executable = sys.executable
-        rel_rez_bin_paths = maker.make_multiple(
-            specifications=get_specifications().values(),
-            options=dict(interpreter_args=["-E"])
-        )
 
-        # Compute relative install path, to work with wheel.
-        # Install path, e.g. "bin/rez" or "scripts/rez" on Windows.
-        abs_rez_dir = os.path.join(os.path.dirname(sys.executable), "rez")
-        rel_rez_dir = os.path.relpath(abs_rez_dir, sys.prefix)
+    def run(self):
+        self.patch_rez_binaries()
+        install_data.install_data.run(self)
 
-        self._append([(rel_rez_dir, rel_rez_bin_paths)])
+    def patch_rez_binaries(self):
+        data_files = []
+        for dst, src in self.data_files:
+            if dst.startswith("_production_script_:"):
+                # Compute relative script install path
+                sub_dir = dst.split(":")[-1]
+                abs_dst_dir = os.path.join(self.script_dir, sub_dir)
+                dst = os.path.relpath(abs_dst_dir, self.install_dir)
+            data_files.append((dst, src))
+        self.data_files[:] = data_files
 
 
 setup(
     cmdclass={
-        "build_py": BuildPyWithRezBinsPatch,
+        "build_py": RezBuildPy,
+        "install_data": InstallData,
     },
     **setup_kwargs
 )
